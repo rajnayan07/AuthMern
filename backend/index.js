81b3487b71f1c64b2a9d8fae8756f6f194eb0fd4
Raj Nayan <122800487+rajnayan07@users.noreply.github.com>
2025-02-15 19:09:26 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,28 @@
  const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const AuthRouter = require('./Routes/AuthRouter');
 const ProductRouter = require('./Routes/ProductRouter');
 
+const app = express();
+
+app.use(cors(
+    {
+        origin: 'https://auth-mern-1-ui.vercel.app', 
+        methods: ['GET', 'POST'],   // Allowed methods
+        credentials: true,
+    }
+));
+
+app.use(express.json())
+
+
 require('dotenv').config();
-
-app.use(cors());
-
 require('./Models/db');
 const PORT = process.env.PORT || 8080;
 
-app.get('/ping', (req, res) => {
-    res.send('PONG');
+app.get('/', (req, res) => {
+    res.json("Hello");
 });
 
 app.use(bodyParser.json());
