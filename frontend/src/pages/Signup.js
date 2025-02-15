14b0b04c73fe4c89cc2bda65b5c266c84660dd74
Raj Nayan <122800487+rajnayan07@users.noreply.github.com>
2025-02-15 17:47:30 +0530

    Update Signup.js

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -27,7 +27,7 @@ function Signup() {
             return handleError('name, email and password are required')
         }
         try {
-            const url = ` https://auth-mern-api-silk.vercel.app/auth/signup`;
+            const url = `https://auth-mern-1-api.vercel.app/auth/signup`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
